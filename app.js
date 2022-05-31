fc91a55a74579d1331aaf8d1a24e7899edfe6919
Bilal <99763904+bial9@users.noreply.github.com>
2022-05-31 23:43:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// var a = document.getElementById("main-content").innerHTML
+// var b = a
+// document.write(b);
+
+// var c = document.getElementById("form-content").nodeType
+// console.log(c)
+
+// var d = document.getElementById("lastName").childNodes
+// console.log(d)
+
+// var e = document.getElementById("main-content").childNodes[0]
+// console.log(e)
+
+// var f = document.getElementById("lastName").nextSibling
+// console.log(f)
+
+// var g = document.getElementById("email").parentNode
+// console.log(g)
